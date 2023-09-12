e7f9841006080e0689e9b0df206f1cad3857c157
Andrey Araujo <andreyaraujo.dev@gmail.com>
2023-09-12 11:27:29 -0300

    style: adiciona eslit e prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,38 @@
-import { FileVideo, Github, Upload, Wand2 } from "lucide-react";
-import { Button } from "./components/ui/button";
-import { Separator } from "./components/ui/separator";
-import { Textarea } from "./components/ui/textarea";
-import { Label } from "./components/ui/label";
+import { FileVideo, Github, Upload, Wand2 } from 'lucide-react'
+import { Button } from './components/ui/button'
+import { Separator } from './components/ui/separator'
+import { Textarea } from './components/ui/textarea'
+import { Label } from './components/ui/label'
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "./components/ui/select";
-import { Slider } from "./components/ui/slider";
+} from './components/ui/select'
+import { Slider } from './components/ui/slider'
 
 export function App() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <div className="px-6 py-3 flex items-center justify-between border-b">
+    <div className="flex min-h-screen flex-col">
+      <div className="flex items-center justify-between border-b px-6 py-3">
         <h1 className="text-xl font-bold">upload.ia</h1>
 
         <div className="flex items-center gap-3">
           <span className="text-sm text-muted-foreground">
             Desenvolvido com 💜 no NLW da Rocketseat
-          </span>{" "}
+          </span>{' '}
           <Separator orientation="vertical" className="h-6" />
           <Button variant="outline">
-            <Github className="w-4 h-4 mr-2" />
+            <Github className="mr-2 h-4 w-4" />
             GitHub
           </Button>
         </div>
       </div>
 
-      <main className="flex-1 p-6 flex gap-6">
-        <div className="flex flex-col flex-1 gap-4">
-          <div className="grid grid-rows-2 gap-4 flex-1">
+      <main className="flex flex-1 gap-6 p-6">
+        <div className="flex flex-1 flex-col gap-4">
+          <div className="grid flex-1 grid-rows-2 gap-4">
             <Textarea
               className="resize-none p-4 leading-relaxed"
               placeholder="Inclua o prompt para a IA..."
@@ -45,8 +45,8 @@ export function App() {
           </div>
 
           <p className="text-small text-muted-foreground">
-            Lembre-se: você pode utilizar a variável{" "}
-            <code className="text-violet-400">{"{transcription}"}</code> no seu
+            Lembre-se: você pode utilizar a variável{' '}
+            <code className="text-violet-400">{'{transcription}'}</code> no seu
             prompt para adicionar o conteúdo da transcrição do vídeo
             selecionado.
           </p>
@@ -55,9 +55,9 @@ export function App() {
           <form className="space-y-6">
             <label
               htmlFor="video"
-              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+              className="flex aspect-video cursor-pointer flex-col items-center justify-center gap-2 rounded-md border border-dashed text-sm text-muted-foreground hover:bg-primary/5"
             >
-              <FileVideo className="w-4 h-4" />
+              <FileVideo className="h-4 w-4" />
               Selecione um vídeo
             </label>
             <input
@@ -75,14 +75,14 @@ export function App() {
               </Label>
               <Textarea
                 id="transcription_prompt"
-                className="h-20 leading-relaxed resize-none"
+                className="h-20 resize-none leading-relaxed"
                 placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
               />
             </div>
 
             <Button type="submit" className="w-full">
               Carregar vídeo
-              <Upload className="w-4 h-4 ml-2" />
+              <Upload className="ml-2 h-4 w-4" />
             </Button>
           </form>
 
@@ -114,7 +114,7 @@ export function App() {
                   <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
                 </SelectContent>
               </Select>
-              <span className="block text-xs text-muted-foreground italic">
+              <span className="block text-xs italic text-muted-foreground">
                 Você poderá customizar essa opção em breve
               </span>
             </div>
@@ -122,7 +122,7 @@ export function App() {
             <div className="space-y-4">
               <Label>Temperatura</Label>
               <Slider min={0} max={1} step={0.1} />
-              <span className="block text-xs text-muted-foreground italic leading-relaxed">
+              <span className="block text-xs italic leading-relaxed text-muted-foreground">
                 Valores mais altos tendem a deixar o resultado mais criativo e
                 com possíveis erros.
               </span>
@@ -132,11 +132,11 @@ export function App() {
 
             <Button type="submit" className="w-full">
               Executar
-              <Wand2 className="w-4 h-4 ml-2" />
+              <Wand2 className="ml-2 h-4 w-4" />
             </Button>
           </form>
         </aside>
       </main>
     </div>
-  );
+  )
 }
